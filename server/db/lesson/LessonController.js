cfb6f8fb0f4b0a3c68c14ad504541717392e5f2a
Kevin Arthur <kevin.arthur.roy@gmail.com>
2016-12-03 16:41:54 -0800

    [setup] scaffolded rating and lesson controllers

---
+++ server/db/lesson/LessonController.js
@@ -0,0 +1,18 @@
+const getAllLessons = (req, res) =>{
+  /**
+   * Make request to db to retrieve all lessons
+   * 
+   */ 
+}
+
+const getOneLesson = (req, res) =>{
+  /**
+   * use req.body.param to grab the lessonId then query the db for it
+   * 
+   */ 
+}
+
+module.exports = (app, express)=>{
+  return {
+  }
+}
---
+++ server/db/rating/RatingController.js
@@ -0,0 +1,11 @@
+const getRating = (req, res) =>{
+  /**
+   * if @params = lessonId then retrieve lesson rating from db
+   * if @params = userId then retrieve user rating from db 
+   */
+}
+
+module.exports = (app, express)=>{
+  return {
+  }
+}
