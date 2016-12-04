83e297a9462f8069e0dbca4fb690205eb14fb851
Kevin Amirdjanian <kevin.amirdjanian@gmail.com>
2016-12-03 16:27:47 -0800

    [setup] scaffolded request controller

---
+++ server/db/request/RequestController.js
@@ -0,0 +1,22 @@
+const retrieveAllRequests = (req, res) =>{
+  /** retrieve request data from db */
+}
+
+const addOneRequest = (req, res) =>{
+  /** add one request to db */
+}
+
+const updateOneRequest = (req, res) =>{
+  /** 
+   * grab the request based on get params from db and update it 
+   * e.g. req.body.params
+   */ 
+}
+
+module.exports = (app, express)=>{
+  return {
+    retrieveAllRequests: retrieveAllRequests,
+    addOneRequest: addOneRequest,
+    updateOneRequest: updateOneRequest
+  }
+}
