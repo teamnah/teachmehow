6192998266bb190444d4cb3e179185cbaf70abdf
Nathaniel Hart <Nathaniel Hart>
2016-12-06 14:50:23 -0800

    [prog] profile information appearing on profile screen

--- client/app/components/prof/prof.controller.js
+++ client/app/components/prof/prof.controller.js
@@ -1,21 +1,55 @@
 
 angular
 .module('app.prof', [])
-.controller('ProfCtrl',function($state, $stateParams, Helpers){
+.controller('ProfCtrl',function($state, $stateParams, authService, Helpers){
   vm = this;
   vm.cache = {};
   vm.cache = Helpers.getCache();
-  //console.log("state params", $stateParams);
-  vm.id = $stateParams.input;
+  // console.log("state params", $stateParams);
+  vm.profile = vm.cache.Users.filter(user=>{
+    return user.id === +$stateParams.input;
+  })[0]
+  console.log("viewing profile for:",vm.profile)
+  vm.picture = vm.profile.picture || "http://victory-design.ru/sandbox/codepen/profile_card/avatar.svg"
+  vm.title = vm.profile.bio ? vm.profile.bio.title : "Knowledge Enthusiast"
+  vm.blurb = vm.profile.bio ? vm.profile.bio.blurb : "TeachMeHow.com member since "+vm.profile.createdAt;
+  vm.myProfile = vm.profile.id === authService.showCurrent().id
 
   vm.lessons = vm.cache.LessByUser.filter(lesson=>{
-    if(lesson.name===vm.id){
+    if(lesson.id===vm.profile.id){
       return lesson.Lessons;
     }
   })[0].Lessons
   
-  vm.bio = vm.cache.Users
+
   console.log("VM id = ", vm.id);
   console.log("lessons by user from cache", vm.lessons)
+  vm.updateProf = function() {
+    console.log('update prof')
+  }
+  vm.updateField = function(field){
+
+  }
+
   return vm;
+
 })
+
+  // $timeout(()=>{
+  //   if(Object.keys(Helpers.getCache()).length === 0){
+  //     Helpers.init()
+  //     .then(()=>{
+  //       vm.teacher = Helpers.getCache()
+  //                   .LessByUser
+  //                   .filter(user=>{
+  //                     if(user.id===Number(vm.teacherId)) return user;
+  //                   })[0];
+  //     });
+  //   }else{
+  //     vm.teacher = Helpers.getCache()
+  //                   .LessByUser
+  //                   .filter(user=>{
+  //                     if(user.id===Number(vm.teacherId)) return user;
+  //                   })[0];
+  //   }
+  // }, 500)
