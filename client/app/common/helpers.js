5fefc81403b65c74451e90c274ae916b53d61e3e
Khoa Nguyen <knb.nguyen8@gmail.com>
2016-12-05 16:37:49 -0800

    added helpers for client

--- client/app/common/helpers.js
+++ client/app/common/helpers.js
@@ -2,7 +2,57 @@
 
 angular
 .module('app.helpers', [])
-.factory('Helpers',function(){
+.factory('Helpers',function($http){
+  /**
+   * If no id is given to these functions it will return everything
+   */
+  const getLessons = (lessonId)=>{
+    return $http.get('/api/lessons',{
+      params: {
+        lessonId: lessonId
+      }
+    })
+  }
 
+  const getCategory = (categoryId)=>{
+    return $http.get('/api/category',{
+      params: {
+        id: categoryId
+      }
+    })
+  }
+
+  const getUsers = (userId)=>{
+    return $http.get('/api/lessons',{
+      params: {
+        id: userId
+      }
+    })
+  }
+
+  const getlessByCat = (categoryId)=>{
+    return $http.get('/api/lessByCat',{
+      params: {
+        id: categoryId
+      }
+    })
+  }
+
+  const getlessByUser = (userId)=>{
+    return $http.get('/api/lessByUser',{
+      params: {
+        id: userId
+      }
+    })
+  }
+
+
+  return {
+    getLessons: getLessons,
+    getCategory: getCategory,
+    getUsers: getUsers,
+    getlessByCat: getlessByCat,
+    getlessByUser: getlessByUser
+  }
   
 })
