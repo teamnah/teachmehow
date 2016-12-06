1e5bdb3ca685ef05461de657bc75d3dc6f207578
Nathaniel Hart <Nathaniel Hart>
2016-12-06 11:47:50 -0800

    [update] setup for lesson feature

---
+++ client/app/components/lesson/lesson.controller.js
@@ -0,0 +1,21 @@
+
+angular
+.module('app.lesson', [])
+.controller('LessonCtrl',function($state, $stateParams, Helpers){
+  vm = this;
+  vm.cache = {};
+  vm.cache = Helpers.getCache();
+  //console.log("state params", $stateParams);
+  vm.id = $stateParams.input;
+
+  vm.lessons = vm.cache.LessByUser.filter(lesson=>{
+    if(lesson.name===vm.id){
+      return lesson.Lessons;
+    }
+  })[0].Lessons
+  
+  vm.bio = vm.cache.Users
+  console.log("VM id = ", vm.id);
+  console.log("lessons by user from cache", vm.lessons)
+  return vm;
+})
