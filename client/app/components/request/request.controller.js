db9d89838ab0a65584edcd3d01eb19458f63b47d
Kevin Amirdjanian <kevin.amirdjanian@gmail.com>
2016-12-05 16:37:50 -0800

    [prog] progress front end request controller

--- client/app/components/request/request.controller.js
+++ client/app/components/request/request.controller.js
@@ -1,8 +1,31 @@
 
 angular
-.module('app.request', [])
-.controller('RequestCtrl',function(){
+.module('app.request', []) 
+.controller('RequestCtrl', function($http, DashService){
   vm = this;
-
+  vm.requests = [];
+  // vm.getRequests = function() {
+    DashService
+      .getAllRequests()
+  // };
   return vm;
 })
+.factory('DashService', function($http) {
+  const getAllRequests = function() {
+    return $http({
+      method: 'GET',
+      url: '/api/requests'
+    })
+    .then(function(requests) {
+      vm.requests = requests;
+      console.log('Here are the requests', vm.requests);
+    })
+    .catch(function(err) {
+      console.log('Error retrieving requests.');
+    })
+  };
+
+  return {
+    getAllRequests: getAllRequests
+  };
+});
