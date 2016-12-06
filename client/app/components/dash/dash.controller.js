be77fb170c87b8caa1bc099b594196c71da44234
Kevin Amirdjanian <kevin.amirdjanian@gmail.com>
2016-12-05 21:46:34 -0800

    [prog] teacher lessons rendering on dash

--- client/app/components/dash/dash.controller.js
+++ client/app/components/dash/dash.controller.js
@@ -1,26 +1,16 @@
 
 angular
 .module('app.dash', [])
-.controller('DashCtrl', function(Helpers, $scope, $state){
+.controller('DashCtrl', function($scope, $stateParams, Helpers){
   vm = this;
-  /**
-   * Currently manually passing in a teacher id
-   * In practice, would grab the input off of $scope
-   * and pass in as the parameter to getlessByUser
-   * 
-   * vm.teacherId = $scope.userInput; 
-   */
-  vm.teacherId = 4;
+  vm.teacherId = $stateParams.input;
   vm.teacherDetails;
 
-  
-
-  console.log('Here we are accessing the cache', vm.cache);
+  Helpers
+    .getlessByUser(vm.teacherId)
+    .then(function(teacherData) {
+      vm.teacherDetails = teacherData.data;
+    })
 
   return vm;
 })
-
-
-// pass in $state, $stateParams
-// grab input and pass into getlessByUser
-// returns a promise and then update the view with the teachers details
