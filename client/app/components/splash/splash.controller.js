f81d1573e259e4881a14bd57182a15bf09b566ae
Nathaniel Hart <Nathaniel Hart>
2016-12-07 10:18:18 -0800

    [feat] auth lockout on splash page

--- client/app/components/splash/splash.controller.js
+++ client/app/components/splash/splash.controller.js
@@ -2,19 +2,35 @@
 
 angular
 .module('app.splash', ['datatables'])
-.controller('SplashCtrl',function($http,Helpers,$state){
+.controller('SplashCtrl',function($http,Helpers,$state, authService){
   vm = this;
   vm.cache = {};
   vm.cache = Helpers.getCache();
   
   vm.goLesson = (input) =>{
-    console.log(input);
-    $state.go("lesson",{input: input})
+    if (!authService.showCurrent()) {
+      swal({
+        title: "Invalid Login",
+        text: "Please log in to view lesson details.",
+        imageUrl: "http://vignette2.wikia.nocookie.net/youtubepoop/images/4/4e/Tubby.png/revision/latest?cb=20140517044848"
+      });
+    } else {
+      console.log(input);
+      $state.go("lesson",{input: input})
+    }
   }
   
   vm.goProfile = (input) =>{
-    console.log(input);
-    $state.go("prof",{input: input})
+    if (!authService.showCurrent()) {
+      swal({
+        title: "Invalid Login",
+        text: "Please log in to view teacher details.",
+        imageUrl: "http://vignette2.wikia.nocookie.net/youtubepoop/images/4/4e/Tubby.png/revision/latest?cb=20140517044848"
+      });
+    } else {
+      console.log(input);
+      $state.go("prof",{input: input})
+    }
   }
   return vm;
 })
