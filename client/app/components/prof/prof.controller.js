1e9c451950691dbe1ef68ae798611cacb049a4ef
Nathaniel Hart <Nathaniel Hart>
2016-12-06 18:11:49 -0800

    [feat] profiles are now updateable

--- client/app/components/prof/prof.controller.js
+++ client/app/components/prof/prof.controller.js
@@ -1,7 +1,7 @@
 
 angular
 .module('app.prof', [])
-.controller('ProfCtrl',function($state, $stateParams, authService, Helpers){
+.controller('ProfCtrl',function($state, $stateParams, $scope, $http, authService, Helpers){
   vm = this;
   vm.cache = {};
   vm.cache = Helpers.getCache();
@@ -11,18 +11,24 @@ angular
   })[0]
   console.log("viewing profile for:",vm.profile)
   vm.picture = vm.profile.picture || "http://victory-design.ru/sandbox/codepen/profile_card/avatar.svg"
-  vm.title = vm.profile.bio ? vm.profile.bio.title : "Knowledge Enthusiast"
-  vm.blurb = vm.profile.bio ? vm.profile.bio.blurb : "TeachMeHow.com member since "+vm.profile.createdAt;
-  vm.myProfile = vm.profile.id === authService.showCurrent().id
-
+  vm.title = vm.profile.bio && JSON.parse(vm.profile.bio).title? 
+    JSON.parse(vm.profile.bio).title : 
+    "Knowledge Enthusiast"
+  $scope.title = vm.title;
+  vm.blurb = vm.profile.bio && JSON.parse(vm.profile.bio).blurb ? 
+    JSON.parse(vm.profile.bio).blurb : 
+    "TeachMeHow.com member since\n"+vm.profile.createdAt;
+  
+  if (authService.showCurrent()){
+    vm.myProfile = vm.profile.id === authService.showCurrent().id
+  }
+  
   vm.lessons = vm.cache.LessByUser.filter(lesson=>{
     if(lesson.id===vm.profile.id){
       return lesson.Lessons;
     }
   })[0].Lessons
   
-
-  console.log("VM id = ", vm.id);
   console.log("lessons by user from cache", vm.lessons)
   vm.updateProf = function() {
     console.log('update prof')
@@ -31,6 +37,28 @@ angular
 
   }
 
+  $scope.$watch('vm.title', function(newVal, oldVal) {
+    if (newVal !== oldVal) {
+      vm.profile.bio && JSON.parse(vm.profile.bio).title?
+        vm.profile.bio.title = newVal :
+        vm.profile.bio = {title: newVal};
+      console.log(vm.profile.bio)
+      $http.put('/api/users/'+vm.profile.id, vm.profile.bio)
+      .then(resp => console.log(resp))
+    }
+  });
+
+  $scope.$watch('vm.blurb', function(newVal, oldVal) {
+    if (newVal !== oldVal) {
+      vm.profile.bio && JSON.parse(vm.profile.bio).blurb?
+        vm.profile.bio.blurb = newVal :
+        vm.profile.bio = {blurb: newVal};
+      console.log("$watch:",vm.profile.bio)
+      $http.put('/api/users/'+vm.profile.id, vm.profile.bio)
+      .then(resp => console.log(resp))
+    }
+  });
+  
   return vm;
 
 })
