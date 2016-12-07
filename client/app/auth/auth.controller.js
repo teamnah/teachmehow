969449dce819f43f4e25b74f2a3a7f654ee44c85
Nathaniel Hart <Nathaniel Hart>
2016-12-06 20:42:29 -0800

    [feat] ability to become a teacher implemented

--- client/app/auth/auth.controller.js
+++ client/app/auth/auth.controller.js
@@ -1,7 +1,18 @@
 angular.module('teachMe')
-.controller('AuthCtrl', function($scope, authService) {
+.controller('AuthCtrl', function($scope, $state, $timeout, authService) {
   let vm=this;
   vm.authService = authService;
   authService.registerAuthListener();
+  $timeout(()=>{
+    vm.isTeach = authService.showCurrent().teachFlag
+  }, 100)
+
+  vm.becomeTeacher = () => {
+    authService.becomeTeacher()
+    .then(()=>{
+      vm.isTeach = true;
+      $state.go('dash')
+    })
+  }
   return vm;
 })
