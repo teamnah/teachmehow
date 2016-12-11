d6957f25c1f7cd1951a8a7d0b2a3503bd9c38dae
jacuchilla <jcuchilla.csupomona@gmail.com>
2016-12-10 20:52:48 -0800

    [progress] Progress on classroom page and load issues

--- client/app/components/dash/dash.controller.js
+++ client/app/components/dash/dash.controller.js
@@ -1,82 +1,80 @@
 angular
-.module('app.dash', ['datatables'])
-.controller('DashCtrl', function($scope, $state, $stateParams, authService, Helpers, $timeout, $http) {
-  if (!authService.showCurrent()) {
-    $state.go('splash');
-  };
-  let vm = this;
-  vm.lesson = {
-    name: '',
-    category: '',
-    details: ''
-  };
-  vm.teacherId = authService.showCurrent().id;
-  vm.goLesson = (input) => {
-    console.log(input);
-    $state.go("lesson", {
-      input: input
-    });
-  };
-  vm.initDash = () => {
-    vm.teacher = Helpers
-      .getCache()
-      .LessByUser
-      .filter(user => {
-        if(user.id === +vm.teacherId) return user;
-      })[0];
-  };
+  .module('app.dash', ['datatables'])
+  .controller('DashCtrl', function ($scope, $state, $stateParams, authService, Helpers, $timeout, $http) {
+    if (!authService.showCurrent()) {
+      $state.go('splash');
+    }
+    let vm = this;
+    vm.lesson = {
+      name: '',
+      category: '',
+      details: ''
+    };
+    vm.teacherId = authService.showCurrent().id;
+    vm.goLesson = (input) => {
+      console.log(input);
+      $state.go('lesson', {
+        input: input
+      });
+    };
+    vm.initDash = () => {
+      vm.teacher = Helpers
+        .getCache()
+        .LessByUser
+        .filter(user => {
+          if (user.id === +vm.teacherId) return user;
+        })[0];
+    };
 
-	vm.submitForm = function(isValid) {
-		if (isValid) {
-      let body = {
-        userName: vm.teacher.name,
-        name: vm.lesson.name,
-        category: vm.lesson.category,
-        details: vm.lesson.details
-      };
-      $http.post('/api/lessons', body)
-      .then(result => {
-        console.log("Response from server:", result);
-        vm.lesson.$setPristine;
-        for(let key in vm.lesson) {
-          vm.lesson[key] = '';
+    vm.submitForm = function (isValid) {
+      if (isValid) {
+        let body = {
+          userName: vm.teacher.name,
+          name: vm.lesson.name,
+          category: vm.lesson.category,
+          details: vm.lesson.details
         };
-        /**
-         * reinitialize cache since database has changed
-         */
+        $http.post('/api/lessons', body)
+          .then(result => {
+            console.log('Response from server:', result);
+            vm.lesson.$setPristine;
+            for (let key in vm.lesson) {
+              vm.lesson[key] = '';
+            }
+            /**
+             * reinitialize cache since database has changed
+             */
+            Helpers.init()
+              .then(() => {
+                vm.initDash();
+              });
+          })
+          .catch(err => {
+            console.log('Error creating new lesson', err);
+            vm.lesson.$setPristine;
+            for (let key in vm.lesson) {
+              vm.lesson[key] = '';
+            }
+          });
+      } else {
+        let invalid = '';
+        for (let key in vm.lesson) {
+          if (vm.lesson[key].length === 0) {
+            invalid += key + ', ';
+          }
+        }
+        window.alert('To create a new lesson, please correct the following: \n' + invalid.substr(0, invalid.length - 2));
+      }
+    };
+
+    if (Object.keys(Helpers.getCache()).length === 0) {
         Helpers.init()
-        .then(() => {
-          vm.initDash();
-        });
-      })
-      .catch(err => {
-        console.log("Error creating new lesson", result);
-        vm.lesson.$setPristine;
-        for(let key in vm.lesson){
-          vm.lesson[key] = '';
-        };
-      });
-		} else {
-      let invalid = "";
-      for(let key in vm.lesson) {
-        if(vm.lesson[key].length === 0) {
-          invalid += key+", ";
-        };
-      };
-      alert("To create a new lesson, please correct the following: \n"+invalid.substr(0, invalid.length-2));
-    };
-	};
-
-  $timeout(() => {
-    if(Object.keys(Helpers.getCache()).length === 0) {
-      Helpers.init()
-      .then(()=>{
+          .then(() => {
+            vm.initDash();
+          });
+      } else {
         vm.initDash();
-      });
-    } else {
-      vm.initDash();
-    };
-  }, 500);
+      }
 
-  return vm;
-});
+    return vm;
+  });
--- client/app/components/splash/splash.controller.js
+++ client/app/components/splash/splash.controller.js
@@ -1,34 +1,43 @@
 angular
 .module('app.splash', ['datatables'])
-.controller('SplashCtrl',function($http,Helpers,$state, authService){
-  vm = this;
-  vm.cache = {};
-  vm.cache = Helpers.getCache();
-  
+.controller('SplashCtrl', function ($http, Helpers, $state, authService) {
+  const vm = this;
+  vm.cache;
+
   vm.goLesson = (input) => {
     if (!authService.showCurrent()) {
       swal({
-        title: "Invalid Login",
-        text: "Please log in to view lesson details.",
-        imageUrl: "http://vignette2.wikia.nocookie.net/youtubepoop/images/4/4e/Tubby.png/revision/latest?cb=20140517044848"
+        title: 'Invalid Login',
+        text: 'Please log in to view lesson details.',
+        imageUrl: 'http://vignette2.wikia.nocookie.net/youtubepoop/images/4/4e/Tubby.png/revision/latest?cb=20140517044848'
       });
     } else {
       console.log(input);
-      $state.go("lesson",{input: input})
-    };
+      $state.go('lesson', {input: input});
+    }
   };
-  
+
   vm.goProfile = (input) => {
     if (!authService.showCurrent()) {
       swal({
-        title: "Invalid Login",
-        text: "Please log in to view teacher details.",
-        imageUrl: "http://vignette2.wikia.nocookie.net/youtubepoop/images/4/4e/Tubby.png/revision/latest?cb=20140517044848"
+        title: 'Invalid Login',
+        text: 'Please log in to view teacher details.',
+        imageUrl: 'http://vignette2.wikia.nocookie.net/youtubepoop/images/4/4e/Tubby.png/revision/latest?cb=20140517044848'
       });
     } else {
       console.log(input);
-      $state.go("prof",{input: input})
-    };
+      $state.go('prof', {input: input});
+    }
   };
+
+  if (!Object.keys(Helpers.getCache()).length) {
+    Helpers.init()
+      .then(() => {
+        vm.cache = Helpers.getCache();
+      });
+  } else {
+    vm.cache = Helpers.getCache();
+  }
+
   return vm;
 });
