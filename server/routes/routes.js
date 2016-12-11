f9a285e5c6de9d9124633f7b748afe9eafb813a9
Mark Steadman <r.m.steadman@gmail.com>
2016-12-10 19:36:48 -0800

    [setup] Adds routes for bookings, get and post

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -11,73 +11,76 @@ const lessByController = require('../db/lessonBy/lessonByController.js');
 
 router
   .route('/users')
-    .get(userController.getAllUsers)
-    .post(userController.addOneUser);
+  .get(userController.getAllUsers)
+  .post(userController.addOneUser);
 
 router
   .route('/users/:userId')
-    .get(userController.getUserDetails);
+  .get(userController.getUserDetails);
 
 router
   .route('/users/:userId/bio')
-    .put(userController.updateUserBio);
+  .put(userController.updateUserBio);
 
 router
   .route('/users/:userId/teach')
-    .put(userController.updateUserTeachFlag);
+  .put(userController.updateUserTeachFlag);
 
 router
   .route('/users/:userId/name')
-    .put(userController.updateUserName);
+  .put(userController.updateUserName);
 
 router
   .route('/login')
-    .post(userController.findUser);
+  .post(userController.findUser);
 
 router
   .route('/lessons')
-    /** 
-     * userId may be passed as params attached to the 
-     * request object on a get request. If so, it will be used to get 
-     * avilable lessons filtered by the userId so that they
-     * may be presented on the teacher dashboard.
-     */ 
-    .get(lessonController.getLesson)
-    .post(lessonController.addLesson)
-    .put(lessonController.updateLesson);
+  /**
+   * userId may be passed as params attached to the
+   * request object on a get request. If so, it will be used to get
+   * avilable lessons filtered by the userId so that they
+   * may be presented on the teacher dashboard.
+   */
+  .get(lessonController.getLesson)
+  .post(lessonController.addLesson)
+  .put(lessonController.updateLesson);
 
 router
   .route('/ratings/:lessonId')
-    .get(ratingController.getRating)
-    .post(ratingController.addRating);
+  .get(ratingController.getRating)
+  .post(ratingController.addRating);
 
 router
   .route('/requests')
-    /** 
-     * userId may be passed as params attached to the 
-     * request object on a get request. If so, it will be used to get 
-     * lessons requested by students and filtered by the teacher's 
-     * category so that they may be presented on the teacher dashboard.
-     */ 
-    .get(requestController.getAllRequests)
-    .post(requestController.addOneRequest);
+  /**
+   * userId may be passed as params attached to the
+   * request object on a get request. If so, it will be used to get
+   * lessons requested by students and filtered by the teacher's
+   * category so that they may be presented on the teacher dashboard.
+   */
+  .get(requestController.getAllRequests)
+  .post(requestController.addOneRequest);
 
 router
   .route('/requests/:requestId')
-    .put(requestController.updateOneRequest);
+  .put(requestController.updateOneRequest);
 
 router
   .route('/category')
-    .get(categoryController.getCat);
+  .get(categoryController.getCat);
 
 router
   .route('/lessByCat')
-    .get(lessByController.lessByCat);
+  .get(lessByController.lessByCat);
 
 router
   .route('/lessByUser')
-    .get(lessByController.lessByUser);
+  .get(lessByController.lessByUser);
 
-
+router
+  .route('/bookings')
+  .get(bookingController.getBooking)
+  .post(bookingController.addBooking);
 
 module.exports = router;
