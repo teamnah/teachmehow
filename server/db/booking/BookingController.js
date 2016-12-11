8f98f3c39fe085c9a297b4eaab19dfc993f1d2a1
Mark Steadman <r.m.steadman@gmail.com>
2016-12-10 19:34:37 -0800

    [setup] Creates addBooking functionality

--- server/db/booking/BookingController.js
+++ server/db/booking/BookingController.js
@@ -33,5 +33,63 @@ module.exports = {
           throw err;
         });
     }
+  },
+
+  addBooking: (req, res, next) => {
+    console.log('req', req.body);
+    let temp = {
+      UserId: null,
+      LessonId: null
+    };
+    if (req.body.UserId === undefined) {
+      res.json([]);
+      return;
+    }
+    models.User.find({
+      where: {
+        id: req.body.UserId
+      }
+    })
+      .then((user) => {
+        temp.UserId = user.dataValues.id;
+        console.log('UserId', temp.UserId);
+        /**
+         * findOrCreate returns a promise that consists of two values in an array
+         * the first value is the returned instance
+         * the second value is a boolean indicating whether the instance was created or note
+         * true --> instance created
+         * false --> instance found
+         * */
+        return models.Lesson.findOrCreate({
+          where: {
+            id: req.body.LessonId
+          },
+          defaults: {
+            id: req.body.LessonId
+          }
+        });
+      })
+      .then((lesson) => {
+        console.log('lesson:', lesson);
+        temp.LessonId = lesson[0].dataValues.id;
+        return models.Booking.create({
+          accept: req.body.accept,
+          details: req.body.details,
+          Spare1: req.body.Spare1,
+          UserId: temp.UserId,
+          LessonId: temp.LessonId
+        });
+      })
+      .then((booking) => {
+        if (booking) {
+          res.json(booking);
+        } else {
+          res.json([]);
+        }
+      })
+      .catch((err) => {
+        res.json(err);
+        throw err;
+      });
   }
 };
