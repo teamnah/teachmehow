5b76b8aa5c7a3824449a88b198af0d99016505bd
Mark Steadman <r.m.steadman@gmail.com>
2016-12-11 13:51:24 -0800

    [setup] Formats serverjs

--- server/server.js
+++ server/server.js
@@ -4,22 +4,22 @@ require('dotenv').config();
  * Compiling all models into the model object
  * all models can now be accessed by models.[modelName]
  */
-const models = require('./config/db.connect.js');  
+const models = require('./config/db.connect.js');
 
 /**
- * Sync all SQL models and THEN start the express server, 
+ * Sync all SQL models and THEN start the express server,
  * otherwise you run the risk of your code running before
- * the models have finished syncing with the database 
- * 
+ * the models have finished syncing with the database
+ *
  */
 models.sequelize.sync().then(() => {
-  const express = require('express'); 
+  const express = require('express');
   const app = express();
-  const port = process.env.PORT || 8080
+  const port = process.env.PORT || 8080;
   const routes = require('./routes/routes.js');
-  require('./config/middleware.js')(app, express); 
+  require('./config/middleware.js')(app, express);
   app.use('/api', routes);
   app.listen(port, () => {
-      console.log("Listening on port " + port);
+    console.log('Listening on port ' + port);
   });
 });
