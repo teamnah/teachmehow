e2ec2a0c4d70dc6cf1232dc95457196e2a873374
jacuchilla <jcuchilla.csupomona@gmail.com>
2016-12-12 15:33:06 -0800

    [feature] Youtube stream auto update channel ids for every lesson

--- client/app/common/youtube/youtube.controller.js
+++ client/app/common/youtube/youtube.controller.js
@@ -6,15 +6,9 @@ angular
   vmYoutube.lessonID = $stateParams.input;
 
   vmYoutube.init = function () {
-    vmYoutube.channelId = Helpers.getCache().Lessons[vmYoutube.lessonID - 1].youtubeKey;
+    let youtubeId = Helpers.getCache().Lessons[vmYoutube.lessonID - 1].youtubeKey;
     console.log('fadsa', vmYoutube.channelId);
-    // console.log('this is vmYoutube in classroom ', vmYoutube.cache);
-    // console.log('heres the id  ', vmYoutube.id);
-  };
-
-  vmYoutube.getChannelId = function () {
-    let channel = $sce.trustAsResourceUrl('https://www.youtube.com/embed/live_stream?channel=' + vmYoutube.channelId);
-    return channel;
+    vmYoutube.channelId = $sce.trustAsResourceUrl('https://www.youtube.com/embed/live_stream?channel=' + youtubeId);
   };
 
   if (Object.keys(Helpers.getCache()).length === 0) {
