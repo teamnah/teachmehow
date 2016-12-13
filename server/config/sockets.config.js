0d1244bbbd3fd59c79b94f782c41b1acb67ebfb5
Jongsoo Yoon <jongsoojong@gmail.com>
2016-12-12 16:20:16 -0800

    [progress] Progress with Persistance save

--- server/config/sockets.config.js
+++ server/config/sockets.config.js
@@ -14,21 +14,13 @@ module.exports = function (server) {
         .then(result => {
           if (result) {
             console.log('THIS IS CHATTTT', data.msg);
-            result.chat.push(JSON.stringify(data.msg));
+            result.chat.push(data.msg);
             console.log('THIS IS RESULT', result.chat);
             console.log('THIS IS DATA', result);
             result.save();
-          } else {
-            res.json([]);
           }
         });
-      // .then(result => {
-      //   res.json(result)
-      // })
-      // .catch(err => {
-      //   res.json(err)
-      //   throw err
-      // })
+
       io.sockets.emit('get-message', data);
 
       console.log('THIS IS DATA', data.id);
